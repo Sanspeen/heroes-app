a882108d01934c1700e160d67ca3687d62bdcae8
Santiago Franco <ktns0930@gmail.com>
2023-03-26 01:30:20 -0500

    Images, title and description loaded.

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,46 @@
 <template>
-  <v-card class="mx-auto" max-width="344">
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-      cover
-    ></v-img>
+  <ul>
+    <li v-for="hero, index in heroes">
+      <v-card class="mx-auto" max-width="344">
+        <v-img
+          :src="hero.thumbnail.path + '.' + hero.thumbnail.extension"
+          height="200px"
+          cover
+        ></v-img>
 
-    <v-card-title> Top western road trips </v-card-title>
+        <v-card-title> {{ hero.name }} </v-card-title>
 
-    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+        <v-card-actions>
+          <v-btn color="orange-lighten-2" variant="text"> Explore </v-btn>
 
-    <v-card-actions>
-      <v-btn color="orange-lighten-2" variant="text"> Explore </v-btn>
+          <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+          <v-btn @click="showDesc()" v-text="show ? '↓' : '→'"></v-btn>
+        </v-card-actions>
 
-      <v-btn @click="showDesc()" v-text="show ? '↓' : '→'"></v-btn>
-    </v-card-actions>
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he
-          could deliver. You won't have time for sleeping, soldier, not with all
-          the bed making you'll be doing. Then we'll go with that data file!
-          Hey, you add a one and two zeros to that or we walk! You're going to
-          do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
+            <v-card-text>
+              {{ hero.description != "" ? hero.description : "This hero is... ZzZzZzzzZzZZz." }}
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-
   name: "IndexPage",
   mounted: function () {
     this.fetchHeroesData();
   },
   data: () => ({
     show: false,
+    heroes: []
   }),
   methods: {
     showDesc() {
@@ -52,7 +50,7 @@ export default {
       const data = await this.$axios.$get(
         "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a159b8cedeb821cb1eb1f1b0406129c1&hash=8204cad645161d8cf403bc8fa5318f1e"
       );
-      const heroes = data.data;
+      this.heroes = data.data.results;
     },
   },
 };
