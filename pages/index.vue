a237de9e1d53b48bfddb9e9d7ce3ac18906d25b1
Santiago Franco <ktns0930@gmail.com>
2023-03-26 01:03:59 -0500

    Card structure created into index.

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,58 @@
 <template>
-  <h1>Hello world</h1>
+  <v-card class="mx-auto" max-width="344">
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+      cover
+    ></v-img>
+
+    <v-card-title> Top western road trips </v-card-title>
+
+    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="orange-lighten-2" variant="text"> Explore </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn @click="showDesc()" v-text="show ? '↓' : '→'"></v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he
+          could deliver. You won't have time for sleeping, soldier, not with all
+          the bed making you'll be doing. Then we'll go with that data file!
+          Hey, you add a one and two zeros to that or we walk! You're going to
+          do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
 
 <script>
 export default {
+
   name: "IndexPage",
   mounted: function () {
     this.fetchHeroesData();
   },
+  data: () => ({
+    show: false,
+  }),
   methods: {
+    showDesc() {
+      this.show = !this.show;
+    },
     async fetchHeroesData() {
       const data = await this.$axios.$get(
         "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a159b8cedeb821cb1eb1f1b0406129c1&hash=8204cad645161d8cf403bc8fa5318f1e"
       );
-      const heroes = data.data
+      const heroes = data.data;
     },
   },
 };
